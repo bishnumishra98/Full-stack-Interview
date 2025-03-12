8e3b0b01ae8541db6aa2923231c39521f47cb77d
Bishnu Mishra <mishravishnu369@gmail.com>
2025-03-12 15:42:24 +0530

    nm

--- JavaScript/01_Hoisting.js
+++ JavaScript/01_Hoisting.js
@@ -4,6 +4,7 @@
 // There are 2 types of hoisting in JavaScript:
 // 1. Variable Hoisting
 // 2. Function Hoisting
+// 3. let and const hoisting
 
 // 1. Variable Hoisting: Variables are hoisted to the top of their containing scope during the compile phase. However,
 // only the declaration is hoisted and not the initialization.
@@ -24,3 +25,13 @@ function greet() {
 // We declared the function greet() after the greet() function call but still it is not throwing any error unlike other
 // programming languages. It is properly showing the output as "Hello world". It is because of hoisting. The function greet()
 // is hoisted to the top of the scope during the compile phase.
+
+// 3. let and const hoisting: let and const are also hoisted to the top of their containing scope during the compile phase.
+// However, the difference is that they are not initialized with undefined. So, if we try to access them before their
+// declaration, it will throw an error. This is because they are in a "temporal dead zone" from the start of the block until
+// the declaration is encountered.
+// Example:
+console.log(y);   // ReferenceError: Cannot access 'y' before initialization
+let y;   // declaration
+// We declared y after the console.log() statement and it is throwing an error because let and const are not initialized with
+// undefined. So, if we try to access them before their declaration, it will throw an error.
