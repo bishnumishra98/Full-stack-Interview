ed754d30fa0b7cbf4ac0f5dccc3830a6732aae0b
Bishnu Mishra <mishravishnu369@gmail.com>
2025-03-15 19:25:13 +0530

    js

---
+++ React/01_VirtualDOM.js
@@ -0,0 +1,6 @@
+// Q) What is the Virtual DOM in React?
+// A) The Virtual DOM (VDOM) is a lightweight copy of the actual DOM in a React application. It is a JavaScript representation
+//    of the real DOM elements and their properties. When changes are made to the React components, the Virtual DOM is updated
+//    first, and then React compares the updated Virtual DOM with the previous Virtual DOM to identify the minimal number of
+//    changes needed to update the actual DOM. This process is known as reconciliation and helps optimize the performance of
+//    React applications by reducing the number of DOM manipulations.
